f3e058cec0a14f4ac3c575733a3c667c766e7f1e
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-12-20 02:02:12 +0200

    fix port

--- apps/frontend/services/api.ts
+++ apps/frontend/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
